cd455d97c471781656c94e9c554487d60025238c
Salman-Irfan <salmanirfan674@gmail.com>
2023-11-02 09:09:51 +0500

    error handling of register and login

--- src/app/services/apiServices/postApiService.js
+++ src/app/services/apiServices/postApiService.js
@@ -4,12 +4,16 @@ import axios from "axios";
 const postApiService = async (formData, endPoint) => {
     try {
         const response = await axios.post(`${BASE_URL}${endPoint}`, formData)
-        console.log(response.data)
         return response.data
     } catch (error) {
-        console.log(error); 
-        alert(error.response.data.errors[0].msg)
-        return error.response.data.errors[0].msg
+        console.log(error);
+        if (endPoint === "/register"){
+            alert(error.response.data.errors[0].msg)
+            return error.response.data.errors[0].msg
+        }else if (endPoint === "/login"){
+            alert(error.response.data.error)
+            return error.response.data.error
+        }
     }
 }
 
